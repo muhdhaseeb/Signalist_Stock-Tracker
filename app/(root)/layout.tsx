6ccd3cd1a657794678446e9ab13ec02a9afee2ee
mohdhaseeb24680 <mohdhaseeb24680@gmail.com>
2026-02-21 22:17:05 +0530

    update

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+const layout = ({ children, }: { children : React.ReactNode}) => {
+  return (
+    <main className="min-h-screen text-gray-400">
+        <header />
+        <div className="container py-10">
+            {children}
+        </div>  
+    </main>
+  )
+}
+
+export default layout
